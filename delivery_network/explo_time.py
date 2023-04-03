71041341958e3e3f4238f1404f9e5560fa86f72d
dtonin <ireni.tonin@ensae.fr>
2023-04-03 13:52:44 +0000

    all fonctions

---
+++ delivery_network/explo_time.py
@@ -0,0 +1,74 @@
+# This python file is used to estimate the time needed to 
+# calculate (using the code developed in Graph.py) the minimum
+# power (and the associated path) over all the paths for each of the given routes.x.in files.
+
+from graph import Graph, graph_from_file
+import time
+from random import randint
+import sys
+sys.setrecursionlimit(5000000)
+
+def mean(L):
+    """ return the average value of a list"""
+    return sum(L)/len(L)
+
+
+# /!\ only for graphs of files in the format network.0x.in :
+# print("There is a path that connects the two nodes !") if Graph.possible_path(g,src,dest) == True else  print("No path possible")
+     
+
+def time_explo(x):
+    """
+    x index of teh file in the format network.x.in and routes.x.in
+    """
+    data_path = "input/"
+    g = graph_from_file(data_path + 'network.'+str(x)+'.in')
+    route_file = data_path + 'routes.'+ str(x) + '.in'
+
+    with open(route_file, "r") as file:
+        n = file.readline().split()
+        n = int(n[0]) # numbers of trips
+        lines =file.readlines() # c'est une liste de lignes ["1 2 3","2 3 5"]
+        l_time = []
+        for line in lines[0:5] :
+            line = line.split()
+            if len(line)==3 :
+                src, dest, weight = map(float,line) # Choice of parameters
+
+                t0 = time.perf_counter()
+                Graph.min_power(g, src, dest)
+                t1 = time.perf_counter()
+
+                t = t1 - t0  # difference between the times before and after execution of the function
+                l_time.append(t)
+            else :
+                    raise Exception("Incorrecte format")
+        print(' INPUT => routes.' + str(x) + '.in, ' + " The average time is : " + str(mean(l_time)) + ' secondes')
+        print(" Approximation of the time needed for all trips is: "+ str(mean(l_time)*n) + ' secondes, càd '+str(mean(l_time)*n/3600)+' heures. \n')
+
+
+for x in range(10,11):
+    time_explo(x)
+
+""" RESULT OF THE PROGRAM: ( note: 3000 hours is equivalent to approximately 83 days) 
+ INPUT => routes.1.in,  The average time is : 0.0005744263995438814 secondes
+ Approximation of the time needed for all trips is: 0.0804196959361434 secondes, càd 2.23388044267065e-05 heures. 
+ INPUT => routes.2.in,  The average time is : 3.509813237603521 secondes
+ Approximation of the time needed for all trips is: 350981.3237603521 secondes, càd 97.49481215565336 heures. 
+ INPUT => routes.3.in,  The average time is : 8.231395321595482 secondes
+ Approximation of the time needed for all trips is: 4115697.660797741 secondes, càd 1143.2493502215946 heures. 
+ INPUT => routes.4.in,  The average time is : 17.89923995300196 secondes
+ Approximation of the time needed for all trips is: 8949619.97650098 secondes, càd 2486.00554902805 heures. 
+ INPUT => routes.5.in,  The average time is : 26.270301272196228 secondes
+ Approximation of the time needed for all trips is: 2627030.127219623 secondes, càd 729.7305908943397 heures. 
+ INPUT => routes.6.in,  The average time is : 21.715683386602905 secondes
+ Approximation of the time needed for all trips is: 10857841.693301452 secondes, càd 3016.067137028181 heures. 
+ INPUT => routes.7.in,  The average time is : 24.826700999808963 secondes
+ Approximation of the time needed for all trips is: 12413350.499904482 secondes, càd 3448.1529166401338 heures. 
+ INPUT => routes.8.in,  The average time is : 16.14622365120449 secondes
+ Approximation of the time needed for all trips is: 8073111.8256022455 secondes, càd 2242.5310626672904 heures. 
+ INPUT => routes.9.in,  The average time is : 17.577709830593086 secondes
+ Approximation of the time needed for all trips is: 8788854.915296543 secondes, càd 2441.348587582373 heures. 
+INPUT => routes.10.in,  The average time is : 16.519155103800585 secondes
+ Approximation of the time needed for all trips is: 8259577.551900293 secondes, càd 2294.3270977500815 heures. 
+"""
---
+++ delivery_network/opti_profit.py
@@ -0,0 +1,142 @@
+"""
+ L' approche théorique la plus instinctive est dans un premier temps de trier les trajets du fichiers
+ route par profit décroissant. Ensuite on parcours chcuns des trajets en lui associant le camion qui coute le moins 
+ chere ayant la puissnace minimale requise. On effectue cette opération jusqu'a épuisement du budget ( ie somme des couts > B)
+"""
+
+# Importation :
+from graph import Graph, graph_from_file, kruskal, oriented_tree_construction, tree_min_power
+import time
+from random import randint
+import sys
+sys.setrecursionlimit(5000000)
+
+
+#################################################################
+#######  1ere METHODE : Une sollution approchée du problème  ####
+#################################################################
+
+
+#  Création des fichier de puissance minimales associées a chaque trajet
+# ( étape effectuée au préalable dans le programme powermin_file )
+
+
+
+###### selction d'un ensemble de camion qui approche une sollution optimal du problème
+
+def trucks_selection(x, Budget = 25*10e9) : 
+    """
+    Cette fonction permet d'effecteur une selection approché de camion qui va permetre de maximiser 
+    l'utilité sous contrainte de budget. On se concentre sur le réseau d'indice x. 
+    args :
+        x(int)        > indice du réseau
+        Budger(float) > budget disponible
+    output :
+        Selection(list)  > liste de la selection de camion proposée
+    """
+    # recupération des fichiers 
+    route_file = 'input/routes.' + str(x) + '.in'
+    powermin_file = 'input/powermin.' + str(x) + '.in'
+    trucks_file = 'input/trucks.' + str(x) + '.in'
+
+    Selection = [] 
+    # Ouverture des fichier et stockage des valeurs dans des listes
+    Trajets = []
+    with open(route_file, "r") as route :
+        with open(powermin_file, "r") as power :
+            n = int(route.readline())
+            for _ in range(n):
+
+                src, dest, utility = map(int, route.readline().split())
+                pwr = int(power.readline())
+                Trajets.append((src, dest, utility, pwr)) 
+                #  il est aussi possible de calculer directement pwr a chaque itération sans passer par lecture des fichier powermin
+
+    Trucks = []      
+    with open(trucks_file, "r") as trucks :   
+        nb_trucks = trucks.readline()
+        for _ in range(int(nb_trucks)) :
+            pow, cost = map(int, trucks.readline().split())
+            Trucks.append((pow, cost))
+
+    #  tri de l'ensemble de trajets par utilité decroissante           
+    Trajets = sorted(Trajets, key=lambda utility:utility[2], reverse = True)
+
+    #  tri du catalogue de camion par couts croissant  
+    Trucks = sorted(Trucks, key=lambda cost:cost[1])
+
+    for trajet in Trajets :
+            for truck in Trucks :
+                if truck[0] >= trajet[3] and Budget - truck[1]>= 0:
+                    chx = truck
+                Budget = Budget - chx[1]
+                Selection.append(chx)
+            print("Budget épuisé. Voici une selection d'achat de camion")
+            return(Selection)
+    print("Budget épuisé. Voici une selection d'achat de camion")
+    return(Selection)
+
+
+print(trucks_selection(1))
+
+# REMARQUE IL EST TRES UTILE DE CREER UNE FONCTION QUI PERMET DE CALCULER L'utilité obtenue a partir d'une selelction de camion 
+# ou bien de directement la calculer ne emme temps que l'on donne la selection. 
+
+
+
+"""
+pseudo-code  explication : 
+on lis en simulatané les fichier route et power et on assoccie les valeusr des deux fichier dnas une liste de tuples L
+on trie la liste obtenue par utilité decroissante 
+on parcours les élemenst de cette liste 
+on trie le fichier trucks par couts décroissant
+tant que le budget est positif >0 :
+    on sélectionne le camion qui coute le moins chere avec une puissance adéquate (on parcours la liste trié et on prend le 1er qui est compatible)
+    condition : Truck[0] >= L[puissancemin]
+        Si  Budget >= Couts :
+            budget = budget - cout
+            trks_selection.append(truck)
+        else : 
+            print("budget épuisé, voici une selction d'achat de camion : ")
+            return trks_selection
+"""
+
+
+############################################################################
+#######  2nd METHODE : Une sollution optimale du problème  (dynamique)  ####
+############################################################################
+
+# Tout d'abord, on recupere l'utilité max que peut generer chaque camion du catalogue
+x=1
+# recupération des fichiers 
+route_file = 'input/routes.' + str(x) + '.in'
+powermin_file = 'input/powermin.' + str(x) + '.in'
+trucks_file = 'input/trucks.' + str(x) + '.in'
+
+Selection = [] 
+# Ouverture des fichier et stockage des valeurs dans des listes
+Trajets = []
+with open(route_file, "r") as route :
+    with open(powermin_file, "r") as power :
+        n = int(route.readline())
+        for _ in range(n):
+
+            src, dest, utility = map(int, route.readline().split())
+            pwr = int(power.readline())
+            Trajets.append((src, dest, utility, pwr)) 
+            #  il est aussi possible de calculer directement pwr a chaque itération sans passer par lecture des fichier powermin
+
+Trucks = []      
+with open(trucks_file, "r") as trucks :   
+    nb_trucks = trucks.readline()
+    for _ in range(int(nb_trucks)) :
+        pow, cost = map(int, trucks.readline().split())
+        umax = 0
+        for trj in Trajets :
+            # si la puissance du c&mion est suffisante et que l'utilité est meilleure on actualise l'utilité max
+            if pow >= trj[3] and umax < trj[2] : 
+                umax = trj[2]
+        # on associe à chaque camion l'utilité max qu'il peut rapporter         
+        Trucks.append((pow, cost, umax))
+
+print(Trucks)
